26edd3528ef353a78d8234464c69f7ae277cedc5
daw2-pereira23 <113383012+daw2-pereira23@users.noreply.github.com>
2023-05-29 19:15:38 +0200

    Update main-021b977e.js

--- assets/main-021b977e.js
+++ assets/main-021b977e.js
@@ -6873,7 +6873,8 @@ const header = {
   template: `
     <nav class="navbar navbar-expand-lg p-3 ">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#"><img src="../imagenes/logo.svg" alt="Logo Svg" id="logotipoPagina"></a>
+    </div>
+  
     </nav>
     `,
   script: () => {
